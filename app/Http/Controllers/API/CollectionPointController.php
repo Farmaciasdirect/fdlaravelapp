392ad6cc2bde9720713d6e19f6442dcbc56af6e1
Sergio Ramirez Cruz <sramirez@farmaciasdirect.com>
2024-04-03 13:44:50 +0200

    Cambio para peticiones por codigo postal

--- app/Http/Controllers/API/CollectionPointController.php
+++ app/Http/Controllers/API/CollectionPointController.php
@@ -8,12 +8,17 @@ use Illuminate\Support\Facades\Cache;
 
 class CollectionPointController extends APIController
 {
-    public function index()
+    public function index(Request $request)
     {
-        $collectionPoints = Cache::remember('collection_points', 360, function () {
-            return CollectionPoint::all();
+        $zipCode = $request->input('zip');
+
+        // Intenta recuperar los datos de la caché utilizando el código postal como clave
+        $collectionPoints = Cache::remember('collection_points_' . $zipCode, 360, function () use ($zipCode) {
+            // Si los datos no están en caché, realiza la consulta a la base de datos filtrando por código postal
+            return CollectionPoint::where('postal_code', $zipCode)->get();
         });
 
+        // Devuelve los puntos de colección en formato JSON
         return response()->json($collectionPoints);
     }
 }
